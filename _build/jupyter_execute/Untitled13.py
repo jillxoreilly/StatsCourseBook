b92a6d1b043804e5bc65e74557902c9a57c81b44
jillxoreilly <102556621+jillxoreilly@users.noreply.github.com>
2023-01-31 01:33:52 +0000

    power analysis add conceptcheck

--- _build/jupyter_execute/Untitled13.py
+++ _build/jupyter_execute/Untitled13.py
@@ -1,64 +1,77 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Hand-in Assignment
+# # Loading data from a .csv file
 # 
-# You task is to produce a report for the Chair of Medicare (the organization that paid for the hospital stays of the patients in this dataset).
-# 
-# The Chair of Medicare wants to know what factors affect the cost of a hospital admission for heart attack.
-# 
-# Your report will summarise the distribution of costs and the factors that affect the cost of a hospital stay, using appropriate plots and descriptive statistics. 
-# 
-# ## Instructions for completing this report
-# 
-# You will produce your report in the form of a a Jupyter notebook (you could modify this one or create a new one).
-# 
-# The notebook will contain text (to be read by the Chair of Medicare who is not familiar with Python code) and code blocks (showing transparently how you processed the data, in case someone in the Medicare Data Science Team wants to check or elaborate on the analysis).
-# 
-# You can add as many new blocks as you need to a notebook by clicking the + button in the toolbar at the top of the notebook viewer, then selecting the type (code or markdown, which is text) from the dropdown menu.
-# 
-# ### What your report should cover
-# 
-# #### Data Overview
-# Report key demographics of the sample (proportion male/female; proportion that survived in each case
-# 
-# You should summarize the distribution of costs, disaggregated (bbroken down) by any categorical variables that, in your opinion, play an important role in determining cost
-# 
-# ## Heart attack data
-# 
-# In this example we will use data from 12,843 patients admitted to hospital in New York City with a heart attack.
-# The data were collected via the Medicare system and are modified from a dataset at <a href="https://dasl.datadescription.com/">DASL</a>
-# 
-# These exercises will review some of the skills learned over the last three weeks. They will also prepare you for the first hand-in exercise: to produce a report for the Chair or Medicare, describing the main factors affecting cost and length of hospital stay for heart attack patients.
-
-# ### Set up Python libraries
-# 
-# As usual, run the code cell below to import the relevant Python libraries
+# This section covers how to load data from a csv file saved on your own computer
+#     
+# You need to do this for the hand-in assignment set in 3rd week so make sure you try it, and check with your tutor in class if stuck.
 
 # In[1]:
 
 
-# Set-up Python libraries - you need to run this but you don't need to change it
+#Set-up Python libraries - you need to run this but you don't need to change it
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 import pandas 
 import seaborn as sns
 sns.set_theme()
+sns.set_style('white')
 
+
+# 
+# In this course you have generally worked with data in Pandas.
+# 
+# To get the data into pandas, you usually run a readymade code block like this:
 
 # In[2]:
 
 
-### Load and inspect the data
+# load the data and have a look
+heartRates = pandas.read_csv('https://raw.githubusercontent.com/jillxoreilly/StatsCourseBook/main/data/HeartRates.csv')
+display(heartRates)
 
+
+# Let's take a closer look at that.
+# 
+# You are using a function called `pandas.read_csv()`
+# 
+# Inside the brackets is a URL for an online file repository (my Github if you are interested), from which the file will be read.
+# 
+# I place all the datafiles for the course on my online repository so I can edit them as needed. However, in 'real life' your data wouldn't be on my github, they would be in a csv file on your own computer.
+# 
+# * Download the datafile CloudSeeding.csv from this week's page on Canvas and place it in the same directory (folder) as the downloaded copy of this Jupyter notebook
+# 
+# Now try running this code block:
 
 # In[3]:
 
 
-heartAttack=pandas.read_csv('data/heartAttack.csv')
-display(heartAttack)
+clouds = pandas.read_csv('CloudSeeding.csv')
+clouds
 
+
+# OOh, it worked!
+# 
+# 
+# ### Subdirectories
+# 
+# Say you have all your Jupyter Notebooks (including this one) in a nice tidy folder (or directory) called `StatsClassWeek3` and you don't want lots of messy csv files lying around in there.
+# 
+# No problem - in your file browser, go to the folder `StatsClassWeek3` and cerate a new folder (or directory) called `data`. Place the csv file `CloudSeeding.csv` in the folder `data`
+# 
+# Now we run the following code:
+# 
+
+# In[4]:
+
+
+clouds = pandas.read_csv('data/CloudSeeding.csv')
+clouds
+
+
+# The slash in the commmand `pandas.read_csv('data/CloudSeeding.csv')` just means that `data` is the name of a folder and `CloudSeeding.csv` ia inside that folder
 
 # In[ ]:
 
